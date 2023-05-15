9974a477c21b84cf10499ba65b22d063a4a9444f
stefanibl <133694715+stefanibl@users.noreply.github.com>
2023-05-15 20:25:43 +0200

    Add files via upload
    testprojwct

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello Intellij IDEA!!!");
+	sayHello("Stefani");
+	sayGoodBye("Monday");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Main(int age) {
+        this.age = age;
+    }
+
+    public static void sayHello(String name){
+        System.out.println("hello" +name);
+    }
+
+    /*
+    safe
+    users
+    bug
+     */
+
+    private int age = 7;
+
+    public static void sayGoodBye(String name){
+        System.out.println("Good Bye " +name);
+        // TODO: Add customer support
+        // da se proveri vtoriot metod
+
+    }
+}
---
+++ src/integrationClass/Firstclass.java
@@ -0,0 +1,9 @@
+package integrationClass;
+
+public class Firstclass {
+    public static void main(String[] args) {
+        System.out.println(
+                "Hello Intellij Idea!!!!");
+    }
+    }
+

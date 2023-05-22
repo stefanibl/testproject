3301ed4bdf522434e473b70b4811dd530126f118
Dell <stefaniblazhevska@gmail.com>
2023-05-22 14:25:02 +0200

    new changes revert

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("new commit");
         System.out.println("Stefani");
         System.out.println("new change");
+        System.out.println("revert");
     }
 
     private int a = 27;

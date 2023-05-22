49589dd76bd4792c94ea63e78a46500a1934092f
Dell <stefaniblazhevska@gmail.com>
2023-05-22 18:44:14 +0200

    New test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println("Stefani");
         System.out.println("new change");
         System.out.println("revert");
+        System.out.println("Hello Ivana");
+        System.out.println("Amend");
     }
 
     private int a = 27;
